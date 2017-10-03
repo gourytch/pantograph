44d3beb7b33ed25fd9ae5afa32280bece3319247
Lev V. Babchenko <gour@mail.ru>
2017-10-04 03:40:49 +0500

    reformat

--- calc.go
+++ calc.go
@@ -3,12 +3,12 @@ package main
 import "math"
 
 type Position struct {
-	X, Y float64 // координата привязки двигающего рычага в пространстве
+	X, Y float64 // координата в пространстве
 }
 type Engine struct {
-	Position  // положение в пространстве
-	R float64 // длина рычага
-	A float64 // нейтральный CCW-поворот рычага от абциссы в радианах
+	Position         // положение оси двигателя/ведущей тяги в пространстве
+	R        float64 // длина ведущей тяги
+	A        float64 // базовый CCW-поворот тяги от абциссы в радианах
 }
 
 type Link struct {
@@ -16,13 +16,13 @@ type Link struct {
 }
 
 type Pantograph struct {
-	E1, E2 Engine // движки
-	L1, L2 float64 // длины ведомых тяг
-	A1, A2 float64 // отклонения ведущих тяг от "нейтрали"
+	E1, E2 Engine   // движки
+	L1, L2 float64  // длины ведомых тяг
+	A1, A2 float64  // отклонения ведущих тяг от "нейтрали"
 	N1, N2 Position // вычисленные координаты концов ведущих тяг
-	P1 Position // вычисленная координата инструментального узла
-	P2 Position // вычисленная координата инструментального узла (альтернативная точка)
-	Valid bool // True если пантограф находится в разрешенном состоянии
+	P1     Position // вычисленная координата инструментального узла
+	P2     Position // вычисленная координата инструментального узла (альтернативная точка)
+	Valid  bool     // True если пантограф находится в разрешенном состоянии
 }
 
 func (p *Pantograph) Solve() {
@@ -31,31 +31,30 @@ func (p *Pantograph) Solve() {
 	// вычислим координаты концов ведущих тяг
 	Sa1, Ca1 := math.Sincos(p.E1.A + p.A1)
 	Sa2, Ca2 := math.Sincos(p.E2.A + p.A2)
-	p.N1.X = p.E1.X + p.L1 * Ca1
-	p.N1.Y = p.E1.Y - p.L1 * Sa1
-	p.N2.X = p.E2.X + p.L2 * Ca2
-	p.N2.Y = p.E2.Y - p.L2 * Sa2
+	p.N1.X = p.E1.X + p.L1*Ca1
+	p.N1.Y = p.E1.Y - p.L1*Sa1
+	p.N2.X = p.E2.X + p.L2*Ca2
+	p.N2.Y = p.E2.Y - p.L2*Sa2
 
 	Dx := p.N2.X - p.N1.X
 	Dy := p.N2.Y - p.N1.Y
 
-	D2 := Dx * Dx + Dy * Dy
+	D2 := Dx*Dx + Dy*Dy
 	D := math.Sqrt(D2)
 
-	// сделаем заглушку позиции в случае разрыва
-	if D > 0 {
-		p.P1.X = p.N1.X + Dx*p.L1/D
-		p.P1.Y = p.N1.Y + Dy*p.L1/D
-		p.P2.X = p.N2.X - Dx*p.L2/D
-		p.P2.Y = p.N2.Y - Dy*p.L2/D
+	if D == 0 {
+		return // совпадение осей. вычислить точку не получится
 	}
+
+	// сделаем заглушку позиции в случае разрыва
+	p.P1.X = p.N1.X + Dx*p.L1/D
+	p.P1.Y = p.N1.Y + Dy*p.L1/D
+	p.P2.X = p.N2.X - Dx*p.L2/D
+	p.P2.Y = p.N2.Y - Dy*p.L2/D
 
 	// теперь вычислим координаты инструментального узла
 	// как точки пересечения двух окружностей
 
-	if D == 0 {
-		return
-	}
 	R12 := p.L1 * p.L1
 	R22 := p.L2 * p.L2
 	A := (R12 - R22 + D2) / (2 * D)
@@ -65,12 +64,11 @@ func (p *Pantograph) Solve() {
 		return
 	}
 	H := math.Sqrt(H2)
-	P2X := p.N1.X + A * Dx / D
-	P2Y := p.N1.Y + A * Dy / D
-	p.P1.X = P2X + (H * Dy) / D
-	p.P1.Y = P2Y - (H * Dx) / D
-	p.P2.X = P2X - (H * Dy) / D
-	p.P2.Y = P2Y + (H * Dx) / D
+	P2X := p.N1.X + A*Dx/D
+	P2Y := p.N1.Y + A*Dy/D
+	p.P1.X = P2X + (H*Dy)/D
+	p.P1.Y = P2Y - (H*Dx)/D
+	p.P2.X = P2X - (H*Dy)/D
+	p.P2.Y = P2Y + (H*Dx)/D
 	p.Valid = true
 }
-
--- main.go
+++ main.go
@@ -2,31 +2,31 @@ package main
 
 import (
 	"image"
+	"math"
 
+	"github.com/fogleman/gg"
 	"golang.org/x/exp/shiny/driver"
 	"golang.org/x/exp/shiny/screen"
-	"golang.org/x/mobile/event/paint"
-	"golang.org/x/mobile/event/mouse"
 	"golang.org/x/mobile/event/key"
 	"golang.org/x/mobile/event/lifecycle"
-	"github.com/fogleman/gg"
-	"math"
+	"golang.org/x/mobile/event/mouse"
+	"golang.org/x/mobile/event/paint"
 )
 
 var (
-	buf     screen.Buffer
-	win     screen.Window
-	tx      screen.Texture
-	Size    = image.Pt(800, 800)
+	buf  screen.Buffer
+	win  screen.Window
+	tx   screen.Texture
+	Size = image.Pt(800, 800)
 )
 
 var p = &Pantograph{
-	E1 : Engine{Position: Position{X:300.0, Y:400.0}, R: 200.0, A: math.Pi / 2.0},
-	E2 : Engine{Position: Position{X:500.0, Y:400.0}, R: 200.0, A: math.Pi / 2.0},
-	L1 : 150.0,
-	L2 : 150.0,
-	A1 : 0.1,
-	A2 : -0.2,
+	E1: Engine{Position: Position{X: 300.0, Y: 400.0}, R: 200.0, A: math.Pi / 2.0},
+	E2: Engine{Position: Position{X: 500.0, Y: 400.0}, R: 200.0, A: math.Pi / 2.0},
+	L1: 150.0,
+	L2: 150.0,
+	A1: 0.1,
+	A2: -0.2,
 }
 
 var drawing = []Position{} // набор точек
@@ -40,23 +40,23 @@ func render(img *image.RGBA) {
 	gc.DrawLine(380, 400, 420, 400)
 	gc.DrawLine(400, 380, 400, 420)
 	gc.DrawCircle(400, 400, 10)
-	gc.SetRGB(0,0,0)
+	gc.SetRGB(0, 0, 0)
 	gc.Stroke()
 
 	// рисуем движки
 	gc.DrawCircle(p.E1.X, p.E1.Y, 5.0)
-	gc.SetRGB(0,0,0)
+	gc.SetRGB(0, 0, 0)
 	gc.Fill()
 	gc.DrawCircle(p.E2.X, p.E2.Y, 5.0)
-	gc.SetRGB(0,0,0)
+	gc.SetRGB(0, 0, 0)
 	gc.Fill()
 
 	// рисуем ведущие тяги
 	gc.DrawLine(p.E1.X, p.E1.Y, p.N1.X, p.N1.Y)
-	gc.SetRGB(0,0,0)
+	gc.SetRGB(0, 0, 0)
 	gc.Stroke()
 	gc.DrawLine(p.E2.X, p.E2.Y, p.N2.X, p.N2.Y)
-	gc.SetRGB(0,0,0)
+	gc.SetRGB(0, 0, 0)
 	gc.Stroke()
 
 	// рисуем ведомые тяги
@@ -106,11 +106,10 @@ func main() {
 					return
 				}
 			case mouse.Event:
-				p.A1 = math.Pi * float64(e.X - 400) / 400
-				p.A2 = math.Pi * float64(e.Y - 400) / 400
+				p.A1 = math.Pi * float64(e.X-400) / 400
+				p.A2 = math.Pi * float64(e.Y-400) / 400
 
 			}
 		}
 	})
 }
-
